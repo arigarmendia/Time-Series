890b0a827c01d68490b8e2efe9f7b08ec6810ee9
Ariadna Garmendia <ari@Ariadnas-MBP.attlocal.net>
2022-10-10 08:37:21 -0500

    Subo ultimas LSTM

--- TP/scripts/my_encoding.py
+++ TP/scripts/my_encoding.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def generate_cyclical_features(df, col_name, period, start_num=0):
     kwargs = {
@@ -6,4 +7,9 @@ def generate_cyclical_features(df, col_name, period, start_num=0):
         f'cos_{col_name}' : lambda x: np.cos(2*np.pi*(df[col_name]-start_num)/period)    
              }
     return df.assign(**kwargs).drop(columns=[col_name])
+
+def onehot_encode(df, col_name):
+    dummies = pd.get_dummies(df[col_name], prefix=col_name)
+    return pd.concat([df, dummies], axis=1).drop(columns=[col_name])
+
 
